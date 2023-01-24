888ca108d4bdfc90b123249d07118e1c5268473e
AlesyaVahrusheva <vahrushevaao@yandex.ru>
2023-01-24 13:56:57 +0300

    Подкрепили файлы

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Принимает 2 числа и выдает наибольшее
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("Наибольшее число: a");
+    return a;
+}
+else
+{
+    Console.Write("Наибольшее число: b");
+    return b;
+}
+
+
